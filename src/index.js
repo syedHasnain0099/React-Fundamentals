c3c3b9fef4369e2ccaadf40bd4172fd4174c1d4a
syedHasnain0099 <syedhasnain0099@gmail.com>
2023-10-05 18:05:36 +0500

    Component Feature

--- src/index.js
+++ src/index.js
@@ -16,43 +16,10 @@ const books = [
     id: 2,
   },
 ];
-const EventExamples = () => {
-  const handleFormInput = (e) => {
-    console.log(e);
-    // e.target - element
-    console.log(`Input Name : ${e.target.name}`);
-    console.log(`Input Value : ${e.target.value}`);
-    // console.log('handle form input');
-  };
-  const handleButtonClick = () => {
-    alert("handle button click");
-  };
-  const handleFormSubmission = (e) => {
-    e.preventDefault();
-    console.log("form submitted");
-  };
-  return (
-    <section>
-      {/* add onSubmit Event Handler */}
-      <form onSubmit={handleFormSubmission}>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={handleFormInput}
-          style={{ margin: "1rem 0" }}
-        />
-        {/* add button with type='submit' */}
-        <button type="submit">submit form</button>
-      </form>
-      <button onClick={handleButtonClick}>click me</button>
-    </section>
-  );
-};
+
 function BookList() {
   return (
     <section className="booklist">
-      <EventExamples />
       {books.map((book) => {
         return <Book {...book} key={book.id} />;
       })}
@@ -62,11 +29,14 @@ function BookList() {
 
 const Book = (props) => {
   const { img, title, author } = props;
-
+  const displayTitle = () => {
+    console.log(title);
+  };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={displayTitle}>display title</button>
       <h4>{author} </h4>
     </article>
   );
